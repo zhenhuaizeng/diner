ae2eb264b2fb18f4487a5e1f40ed77746401c7b2
Jack Zeng <zeng.zhenhuai@student.greenriver.edu>
2023-01-24 17:07:43 -0500

    Added lunch and breakfast pages

--- index.php
+++ index.php
@@ -20,6 +20,26 @@ $f3->route('GET /', function(){
     echo $view -> render("views/diner-home.html");
 });
 
+//Define a breakfast route(328/diner/breakfast)
+$f3->route('GET /breakfast', function(){
+
+    //Instantiate a view
+    $view = new Template();
+    echo $view -> render("views/breakfast.html");
+});
+
+//Define a lunch route(328/diner/breakfast)
+$f3->route('GET /lunch', function(){
+
+    //Instantiate a view
+    $view = new Template();
+    echo $view -> render("views/lunch.html");
+});
+//1. Help each other get caught up
+
+//2. Define a lunch route + page
+
+//3. Add an image to breakfast and/or lunch
 
 //Run Fat Free
 $f3->run();
